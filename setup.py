652d1a9cb47aa85166557c9429f3fd954efb612f
Josh Pattman <josh.pattman@gmail.com>
2021-05-15 20:28:25 +0100

    Fixed README and added manifest

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
+from os import path
 
 def readme():
-    with open("README.md", 'r') as f:
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
         return f.read()
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 setup(name='spotpuppy',
       version=VERSION,
       description='Package for controlling a dynamically balanced quadruped',
+      long_description_content_type='text/markdown',
       long_description=readme(),
       author='Josh Pattman',
       author_email='josh.pattman@gmail.com',
