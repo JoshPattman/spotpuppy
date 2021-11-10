e6a77a4e02987e9a778688aa20ad141109fd1440
Josh Pattman <Josh.pattman@gmail.com>
2021-11-10 21:37:04 +0000

    added basic pid control module

---
+++ spotpuppy/utils/pid_control.py
@@ -0,0 +1,28 @@
+import time
+
+class pid_controller:
+    def __init__(self, Kp, Ki, Kd):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+
+        self.last_error = 0
+        self.integral = 0
+        self.last_time = time.time()
+
+        self.target = 0
+
+    def update(self, val):
+        t = time.time()
+        dt = t - self.last_time
+        self.last_time = t
+
+        error = self.target - val
+        d = (error - self.last_error)/ dt
+        self.integral += error * dt
+        self.last_error = error
+
+        return (self.Kp * error) + (self.Ki * self.integral) + (self.Kd * d)
+
+    def set_target(self, target):
+        self.target = target
