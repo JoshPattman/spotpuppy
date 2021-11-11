ae1526b3c0e082b6eb616134f0660e83817fd1f6
Josh Pattman <Josh.pattman@gmail.com>
2021-11-11 12:47:02 +0000

    Update pid_control.py

--- spotpuppy/utils/pid_control.py
+++ spotpuppy/utils/pid_control.py
@@ -8,7 +8,7 @@ class pid_controller:
 
         self.last_error = 0
         self.integral = 0
-        self.last_time = time.time()
+        self.reset_time()
 
         self.target = 0
 
@@ -18,11 +18,14 @@ class pid_controller:
         self.last_time = t
 
         error = self.target - val
-        d = (error - self.last_error)/ dt
+        d = (error - self.last_error) / dt
         self.integral += error * dt
         self.last_error = error
 
         return (self.Kp * error) + (self.Ki * self.integral) + (self.Kd * d)
+
+    def reset_time(self):
+        self.last_time = time.time()
 
     def set_target(self, target):
         self.target = target
