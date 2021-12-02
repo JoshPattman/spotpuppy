ada82467ae9662c374f3b7c1533b9aad0515d29f
Josh Pattman <Josh.pattman@gmail.com>
2021-12-02 14:39:22 +0000

    tidied constuctor

--- spotpuppy/models/basic_quadruped.py
+++ spotpuppy/models/basic_quadruped.py
@@ -1,8 +1,8 @@
 from . import quadruped_base
 
 class quadruped(quadruped_base.quadruped):
-    def __init__(self, rotation_sensor=None, servo_controller=None, bone_length=6, body_dims=[10, 10], fall_rotation_limit=0):
-        quadruped_base.quadruped.__init__(self, rotation_sensor=rotation_sensor,servo_controller=servo_controller,bone_length=bone_length,body_dims=body_dims,fall_rotation_limit=fall_rotation_limit)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def _on_update(self):
         self.quad_controller.set_all_legs(self.calculate_still_positions())
