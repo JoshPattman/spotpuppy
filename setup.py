47b4f721aa854dd39aaec968505aa263862e90f7
Josh Pattman <Josh.pattman@gmail.com>
2021-12-04 13:49:05 +0000

    Inc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from os import path
 
 GITHUBLINK = "https://github.com/JoshPattman/spotpuppy"
-VERSION = "0.2.2"
+VERSION = "0.2.3"
 
 
 this_directory = path.abspath(path.dirname(__file__))
